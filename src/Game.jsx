10bed0ebaefab377ed7327d35c2228f5f1ffb060
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-25 06:13:01 -0700

    using usestate

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Square from './Square.jsx'
 
 const Game = () => {
+  const [num, setNum] = useState([...Array(9).fill(null)])
+  console.log(num)
   return (
     <div className='container'>
       <div className='row'>
-        <Square />
+        <Square value={num[0]} />
         <Square />
         <Square />
       </div>
