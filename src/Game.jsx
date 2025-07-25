dc5b7767a6e57f1dcb3b2c8e2493e6b972ad7033
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-25 05:07:42 -0700

    import square in to game.jsx

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import Square from './Square.jsx'
 
 const Game = () => {
   return (
     <div className='container'>
-      <div className='row'></div>
-      <div className='row'></div>
-      <div className='row'></div>
+      <div className='row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className='row'>
+        <Square />
+        <Square />
+        <Square />
+      </div>
     </div>
   )
 }
