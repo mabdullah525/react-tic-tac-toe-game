b0d0b366143670b246fbfd294837330e89d8615f
mabdullah525 <abdullahbinabdullah005@gmail.com>
2025-07-25 10:17:28 -0700

    add onclick

--- src/Game.jsx
+++ src/Game.jsx
@@ -4,22 +4,25 @@ import Square from './Square.jsx'
 const Game = () => {
   const [num, setNum] = useState([...Array(9).fill(null)])
   console.log(num)
+  const handleClick = (index) => {
+    console.log('clicked', index)
+  }
   return (
     <div className='container'>
       <div className='row'>
-        <Square value={num[0]} />
-        <Square value={num[1]} />
-        <Square value={num[2]} />
+        <Square onClick={() => handleClick(0)} value={num[0]} />
+        <Square onClick={() => handleClick(1)} value={num[1]} />
+        <Square onClick={() => handleClick(2)} value={num[2]} />
       </div>
       <div className='row'>
-        <Square value={num[3]} />
-        <Square value={num[4]} />
-        <Square value={num[5]} />
+        <Square onClick={() => handleClick(3)} value={num[3]} />
+        <Square onClick={() => handleClick(4)} value={num[4]} />
+        <Square onClick={() => handleClick(5)} value={num[5]} />
       </div>
       <div className='row'>
-        <Square value={num[6]} />
-        <Square value={num[7]} />
-        <Square value={num[8]} />
+        <Square onClick={() => handleClick(6)} value={num[6]} />
+        <Square onClick={() => handleClick(7)} value={num[7]} />
+        <Square onClick={() => handleClick(8)} value={num[8]} />
       </div>
     </div>
   )
